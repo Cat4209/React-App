a7d3b10e2cee02d55f73d0b2847767dc6de9136e
Cat4209 <egorkaratunov2003@gmail.com>
2025-12-13 18:20:35 +0200

    loacalStorage hook added

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function useLocalStorage(initialState, key){
+
+    const [storedValue, setStoredValue] = useState(()=>{
+        try {
+            const item = localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            console.error("Error reading localStorage key", key, error);
+            return initialValue;
+        }
+    });
+
+    useEffect(() => {
+        try {
+        localStorage.setItem(key, JSON.stringify(storedValue));
+        } catch (error) {
+        console.error("Error setting localStorage key", key, error);
+        }
+  }, [key, storedValue]);
+
+  return [storedValue, setStoredValue];
+}
